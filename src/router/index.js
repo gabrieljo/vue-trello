3511f3c823d3818c06a2a72e52b876d16a3b2279
Gabriel Jo <gabrieljo1302@gmail.com>
2019-06-09 12:36:59 +1200

    routing dynamic

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,19 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router';
-import App from '../App.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../components/Home";
+import Login from "../components/Login";
+import Board from "../components/Board";
+import NotFound from "../components/NotFound";
 
 Vue.use(VueRouter);
 
-const Login = { template: '<div>Login Page</div>'}
-const NotFound = {template : '<div>Page not found</div>'}
-
 const routes = [
-  {path: '/', component: App},
-  {path: '/login', component: Login},
-  {path: '*', component: NotFound},
-]
+  { path: "/", component: Home },
+  { path: "/login", component: Login },
+  { path: "/b/:bid", component: Board },
+  { path: "*", component: NotFound }
+];
 
-const router = new VueRouter({ mode:'history', routes })
+const router = new VueRouter({ mode: "history", routes });
 
 export default router;
