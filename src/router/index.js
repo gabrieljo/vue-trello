5588e10e02a051d79fb83545acc9e81808953db7
Gabriel Jo <gabrieljo1302@gmail.com>
2019-06-09 16:35:07 +1200

    navigation guard

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,20 @@ import NotFound from "../components/NotFound";
 
 Vue.use(VueRouter);
 
+const requireAuth = (to, from, next) => {
+  const isAuth = localStorage.getItem("token");
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`;
+  isAuth ? next() : next(loginPath);
+};
+
 const routes = [
-  { path: "/", component: Home },
+  { path: "/", component: Home, beforeEnter: requireAuth },
   { path: "/login", component: Login },
   {
     path: "/b/:bid",
     component: Board,
-    children: [{ path: "c/:cid", component: Card }]
+    beforeEnter: requireAuth,
+    children: [{ path: "c/:cid", component: Card, beforeEnter: requireAuth }]
   },
   { path: "*", component: NotFound }
 ];
