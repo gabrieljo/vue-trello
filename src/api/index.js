7a5019399aa8b3278f9bb74c0104cc13d5bc6811
Gabriel Jo <gabrieljo1302@gmail.com>
2019-06-30 12:47:02 +1200

    add card ui

--- src/api/index.js
+++ src/api/index.js
@@ -28,11 +28,10 @@ export const setAuthInHeader = token => {
 };
 
 export const board = {
-  fetch() {
-    return request("get", "boards");
+  fetch(id) {
+    return id ? request("get", `boards/${id}`) : request("get", "boards");
   },
   create(title) {
-    console.log(title);
     return request("post", "boards", { title });
   }
 };
