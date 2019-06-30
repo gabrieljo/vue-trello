b7e99276e4372a0aaf840d4cfff5847351f3c4da
Gabriel Jo <gabrieljo1302@gmail.com>
2019-06-30 16:36:57 +1200

    add drag and drop dragula

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -21,12 +21,16 @@
 <script>
 import { mapState, mapActions } from "vuex";
 import List from "./List";
+import dragula from "dragula";
+import "dragula/dist/dragula.css";
+
 export default {
   components: { List },
   data() {
     return {
       bid: 0,
-      loading: false
+      loading: false,
+      dragulaCards: null
     };
   },
   computed: {
@@ -34,11 +38,55 @@ export default {
       board: "board"
     })
   },
+  updated() {
+    if (this.dragulaCards) this.dragulaCards.destroy();
+
+    this.dragulaCards = dragula([
+      ...Array.from(this.$el.querySelectorAll(".card-list"))
+    ]).on("drop", (el, wrapper, target, siblings) => {
+      const targetCard = {
+        id: el.dataset.cardId * 1,
+        pos: 65535
+      };
+
+      let prevCard = null;
+      let nextCard = null;
+      Array.from(wrapper.querySelectorAll(".card-item")).forEach(
+        (el, idx, arr) => {
+          const cardId = el.dataset.cardId * 1;
+          if (cardId == targetCard.id) {
+            prevCard =
+              idx > 0
+                ? {
+                    id: arr[idx - 1].dataset.cardId * 1,
+                    pos: arr[idx - 1].dataset.cardPos * 1
+                  }
+                : null;
+
+            nextCard =
+              idx < arr.length - 1
+                ? {
+                    id: arr[idx + 1].dataset.cardId * 1,
+                    pos: arr[idx + 1].dataset.cardPos * 1
+                  }
+                : null;
+          }
+        }
+      );
+      if (!prevCard && nextCard) targetCard.pos = nextCard.pos / 2;
+      else if (!nextCard && prevCard) targetCard.pos = prevCard.pos * 2;
+      else if (prevCard && nextCard)
+        targetCard.pos = (prevCard.pos + nextCard.pos) / 2;
+
+      console.log(targetCard);
+      this.UPDATE_CARD(targetCard);
+    });
+  },
   created() {
     this.fetchData();
   },
   methods: {
-    ...mapActions(["FETCH_BOARD"]),
+    ...mapActions(["FETCH_BOARD", "UPDATE_CARD"]),
     fetchData() {
       this.loading = true;
       this.FETCH_BOARD({ id: this.$route.params.bid }).then(
